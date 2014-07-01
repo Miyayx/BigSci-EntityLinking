0e39be1005ef23faf647b57ce2655af88955e6c1
Miyayx <yangxue00.yxmn@gmail.com>
2014-07-01 19:11:37 +0800

    add tool add split_dump

---
+++ tools/split_dump.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+def split_dump(in_dir, out_dir, dump_f):
+    start_num = 0
+    end_num   = 0
+    n = 1
+    fn = out_dir+dump_f.split(".")[0]+"{:0>2d}.xml-p{:0>10d}p{:0>10d}".format(n, start_num, start_num+100000)
+    f = open(fn,"w")
+    for l in open(in_dir+dump_f):
+        if l.startswith("<mediawiki"): # article start
+            if end_num % 100000 == 0:
+                f.close()
+                n += 1
+                fn = out_dir+dump_f.split(".")[0]+"{:0>2d}.xml-p{:0>10d}p{:0>10d}".format(n,start_num,end_num)
+                start_num = end_num 
+                f = open(fn,"w")
+        if l.startswith("</mediawiki>"): # article end
+            end_num += 1
+
+        f.write(l)
+        f.flush()
+    f.close()
+
+if __name__=="__main__":
+    import os 
+    for f in os.listdir("/home/zigo/sy/etc/dump/"):
+        split_dump("/home/zigo/sy/etc/dump/","/home/zjt/enwikiMap/data/new-wiki-article/",f)
