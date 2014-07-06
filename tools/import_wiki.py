53e30242e301d30ad57310fa4582fbdd9cc7ba46
Miyayx <yangxue00.yxmn@gmail.com>
2014-07-06 14:09:28 +0800

    change import_wiki

--- tools/import_wiki.py
+++ tools/import_wiki.py
@@ -2,15 +2,23 @@
 #-*-coding:utf-8-*-
 
 from nltk.corpus import stopwords
-from ../db.py import *
 
 def get_outof_xlore_list(fold, fnew):
-    olds = [line.strip("\n") for line in open(fold)]
+    newl = []
+    olds = [line.strip("\n").split("\t\t")[1] for line in open(fold)]
     news = [line.strip("\n") for line in open(fnew)]
-    return list(set(news)-set(olds))
+    l = list(set(news)-set(olds))
+    stop = stopwords.words('english')
+    for i in l:
+        if i not in stop:
+            newl.append(i)
+    return newl
 
 if __name__=="__main__":
-    ws = get_outof_xlore_list("/home/zigo/sy/etc/enwiki-title.dat","/home/lsj/data/enwiki/enwiki-instance")
-    print len(ws)
+    ws = get_outof_xlore_list("/home/zjt/enwikiMap/data/enInstIndex.ttl","/home/lsj/data/enwiki/enwiki-instanceList.dat")
+    with open("../data/words_notin_xlore.dat","w") as f:
+        for w in ws:
+            f.write(w+"\n")
+   
 
 
